97d40fc8743c85bbed116fc97c40e1028f9f2a3f
Mariann881018 <mariann1018@hotmail.com>
2019-04-05 11:12:05 +0200

    OneTwoALot added

---
+++ week-01/day-4/OneTwoALot.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class OneTwoALot {
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+
+    System.out.println("Give me a number");
+    int num = scan.nextInt();
+
+    if (num == 0 || num < 0) {
+      System.out.println("Not enough");
+    } else if (num == 1) {
+      System.out.println("One");
+    } else if (num == 2) {
+        System.out.println("Two");
+      } else {
+      System.out.println("A lot");
+    }
+  }
+}
