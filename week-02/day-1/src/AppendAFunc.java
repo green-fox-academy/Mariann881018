c415afad237410fdcc93142df22e90a14fbb8a9b
Mariann881018 <mariann1018@hotmail.com>
2019-04-08 14:35:51 +0200

    AppendAFunc and Greet added

---
+++ week-02/day-1/src/AppendAFunc.java
@@ -0,0 +1,14 @@
+public class AppendAFunc {
+  public static void main(String[] args) {
+    String typo = "Chinchill";
+    String runIt = appendAFunc(typo);
+    System.out.println(runIt);
+  }
+
+    public static String appendAFunc(String a){
+      String makeIt;
+      makeIt = a + "a";
+      return makeIt;
+    }
+  }
+  
--- week-02/day-1/src/Greet.java
+++ week-02/day-1/src/Greet.java
@@ -1,8 +1,8 @@
 public class Greet {
   public static void main(String[] args) {
     String al = "Greenfox";
-    String greeting = greet(al);
-    System.out.println(greeting);
+    String fullGreeting = greet(al);
+    System.out.println(fullGreeting);
   }
   public static String greet(String a){
     String hello;
@@ -11,10 +11,3 @@ public class Greet {
   }
 
 }
-
-
-//  From now on, create the usual class wrapper and main method on your own.
-// - Create a string variable named `al` and assign the value `Greenfox` to it
-// - Create a function called `greet` that greets it's input parameter
-//     - Greeting is printing e.g. `Greetings dear, Greenfox`
-// - Greet `al`
