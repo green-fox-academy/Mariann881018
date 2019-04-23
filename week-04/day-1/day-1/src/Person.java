bfaf88c385ff76d8cbc40c69b01a2bb5e0e1757e
Mariann881018 <mariann1018@hotmail.com>
2019-04-23 11:27:35 +0200

    person created

--- week-04/day-1/day-1/src/Person.java
+++ week-04/day-1/day-1/src/Person.java
@@ -1,36 +1,33 @@
-class Person {
+public class Person {
   String name;
   int age;
   String gender;
 
-  public Person() {
+  public Person(){
     this.name = "Jane Doe";
     this.age = 30;
     this.gender = "female";
-  }
 
-  public Person(String name, int age, String gender) {
+  }
+  public Person(String name, int age, String gender){
     this.name = name;
     this.age = age;
     this.gender = gender;
   }
 
-  void introduce() {
+  void introduce(){
     System.out.println("Hi, I'm " + this.name + ", a " + this.age + " year old " + this.gender);
   }
-
-  void getGoal() {
-    System.out.println("My goal is: Live for the moment!");
-  }
 }
+
 /*
 Create a Person class with the following fields:
  - name: the name of the person
  - age: the age of the person (whole number)
+ - gender: the gender of the person (male / female)
 Constructors:
  - Person(name, age, gender)
  - Person(): sets name to Jane Doe, age to 30, gender to female
- - gender: the gender of the person (male / female)
 Methods:
  - introduce(): prints out "Hi, I'm name, a age year old gender."
  - getGoal(): prints out "My goal is: Live for the moment!"
