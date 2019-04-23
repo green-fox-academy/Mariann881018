294354dc3a89d832d572b0122e6dbdf00e9053ba
Mariann881018 <mariann1018@hotmail.com>
2019-04-23 11:35:41 +0200

    student added

---
+++ week-04/day-1/day-1/src/Student.java
@@ -0,0 +1,33 @@
+public class Student extends Person{
+  String prevOrg;
+  int skippedDays;
+
+  public Student(String name, int age, String gender, String prevOrg){
+    super(name, age, gender);
+    this.prevOrg = prevOrg;
+    int skippedDays = 0;
+  }
+
+  public Student(){
+    this.prevOrg = "The School of Life";
+    this.skippedDays = 0;
+  }
+
+
+}
+/*
+Create a Student class that has the same fields and methods as the Person class, and has the following additional
+fields:
+ - previousOrganization: the name of the student’s previous company / school
+ - skippedDays: the number of days skipped from the course
+
+The Student class has the following constructors:
+ - Student(name, age, gender, previousOrganization): beside the given parameters, it sets skippedDays to 0
+  Student(): sets name to Jane Doe, age to 30, gender to female, previousOrganization to The School of Life, skippedDays to 0
+
+methods:
+ - getGoal(): prints out "Be a junior software developer."
+ - introduce(): "Hi, I'm name, a age year old gender from previousOrganization who skipped skippedDays days from the course already."
+ - skipDays(numberOfDays): increases skippedDays by numberOfDays
+
+ */
