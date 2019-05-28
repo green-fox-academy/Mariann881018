980c34a3ee391f712fc155a20f0d51dedc87a7cc
Mariann881018 <mariann1018@hotmail.com>
2019-05-28 10:43:41 +0200

    Generated exercise project

---
+++ week-08/day-2/src/main/java/ToDosWithDatabase/ListToDosApplication.java
@@ -0,0 +1,13 @@
+package ToDosWithDatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListToDosApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ListToDosApplication.class, args);
+  }
+
+}
